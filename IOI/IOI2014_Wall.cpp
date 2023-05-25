c93895504e932cbd743746a15ea6b7ab87ba259a
Desh Acharjee <134647238+DeshAcharjee@users.noreply.github.com>
2023-05-26 02:07:36 +0600

    Update IOI2014_Wall.cpp

--- IOI/IOI2014_Wall.cpp
+++ IOI/IOI2014_Wall.cpp
@@ -1,207 +1,120 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node {
-    int mn1;
-    int mn2;
-    int mx1;
-    int mx2;
-    int mncnt;
-    int mxcnt;
-    int sum;
-    int lz;
-    node() : mn1(2e9), mn2(2e9), mx1(-2e9), mx2(-2e9), mncnt(0), mxcnt(0), sum(0), lz(0) { };
-};
-
-struct SegmentTreeBeats {
-    vector<node> st;
+struct segtree {
+    vector<int> st, t, o;
     int sz;
-    void build(int v, int l, int r, vector<int> &a) {
-        if (l == r) {
-            st[v].mn1 = a[l];
-            st[v].mx1 = a[l];
-            st[v].mncnt = st[v].mxcnt = 1;
-            st[v].sum = a[l];
-            return;
-        }
-        int m = l + r >> 1;
-        build(v << 1, l, m, a);
-        build(v << 1 | 1, m + 1, r, a);
-        pull(v);
+    segtree(int n) {
+        sz = n;
+        while (__builtin_popcount(sz) ^ 1) sz++;
+        st.resize(sz << 1), o.resize(sz << 1, -1), t.resize(sz << 1);
     }
-    void applyleaf(int v, int x) {
-        st[v].mn1 = x;
-        st[v].mx1 = x;
-        st[v].mncnt = st[v].mxcnt = 1;
-        st[v].sum = x;
-    }
-    SegmentTreeBeats(vector<int> a) {
-        sz = a.size();
-        while (__builtin_popcount(sz) ^ 1) ++sz;
-        st.resize(sz << 1);
-        for (int i = 0; i < a.size(); i++) applyleaf(i + sz, a[i]);
-        for (int i = sz - 1; i; i--) pull(i);
-    }
-    void apply1(int v, int x, int l, int r) {
-        if (x == 0) return;
-        st[v].mx1 += x;
-        if (st[v].mx2 != -2e9) st[v].mx2 += x;
-        st[v].mn1 += x;
-        if (st[v].mn2 != 2e9) st[v].mn2 += x;
-        st[v].sum += (r - l + 1) * x;
-        st[v].lz += x;
-    }
-    void apply2(int v, int x, bool f) {
-        if (st[v].mn1 >= x) return;
-        st[v].sum = st[v].sum - st[v].mncnt * st[v].mn1 + st[v].mncnt * x;
-        if (f) {
-            st[v].mn1 = st[v].mx1 = x;
+    void change(int v, int h, int op) {
+        if (op == 4) {
+            st[v] = h, o[v] = 4;
+        } else if (o[v] == -1) {
+            if (op == 2) {
+                st[v] = 1e9, t[v] = h, o[v] = 1;
+            } else {
+                st[v] = h, t[v] = -1e9, o[v] = 0;
+            }
+        } else if (o[v] == 4) {
+            if (op == 2) {
+                st[v] = max(st[v], h);
+            } else {
+                st[v] = min(st[v], h);
+            }
+            t[v] = st[v];
         } else {
-            st[v].mn1 = x;
-            if (x >= st[v].mx1) st[v].mx1 = x;
-            else if (x > st[v].mx2) st[v].mx2 = x;
+            if (op == 2) {
+                t[v] = max(t[v], h);
+                if (t[v] >= st[v]) {
+                    st[v] = t[v];
+                    o[v] = 4;
+                }
+            } else {
+                st[v] = min(st[v], h);
+                if (t[v] >= st[v]) {
+                    t[v] = st[v];
+                    o[v] = 4;
+                }
+            }
         }
     }
-    void apply3(int v, int x, bool f) {
-        if (st[v].mx1 <= x) return;
-        st[v].sum = st[v].sum - st[v].mxcnt * st[v].mx1 + st[v].mxcnt * x;
-        if (f) {
-            st[v].mx1 = st[v].mn1 = x;
+    void push(int v) {
+        if ((v << 1) >= sz) {
+            if (o[v] == 1) {
+                st[v << 1] = min(st[v << 1], st[v]);
+                st[v << 1] = max(st[v << 1], t[v]);
+                st[v << 1 | 1] = min(st[v << 1 | 1], st[v]);
+                st[v << 1 | 1] = max(st[v << 1 | 1], t[v]);
+            } else if (o[v] == 0) {
+                st[v << 1] = max(st[v << 1], t[v]);
+                st[v << 1] = min(st[v << 1], st[v]);
+                st[v << 1 | 1] = max(st[v << 1 | 1], t[v]);
+                st[v << 1 | 1] = min(st[v << 1 | 1], st[v]);
+            } else {
+                st[v << 1] = st[v];
+                st[v << 1 | 1] = st[v];
+            }
         } else {
-            st[v].mx1 = x;
-            if (x <= st[v].mn1) st[v].mn1 = x;
-            else if (x < st[v].mn2) st[v].mn2 = x;
+            if (o[v] == 1) {
+                change(v << 1, st[v], 3);
+                change(v << 1, t[v], 2);
+                change(v << 1 | 1, st[v], 3);
+                change(v << 1 | 1, t[v], 2);
+            } else if (o[v] == 0) {
+                change(v << 1, t[v], 2);
+                change(v << 1, st[v], 3);
+                change(v << 1 | 1, t[v], 2);
+                change(v << 1 | 1, st[v], 3);
+            } else {
+                change(v << 1, st[v], 4);
+                change(v << 1 | 1, st[v], 4);
+            }
         }
+        o[v] = -1;
     }
-    void push(int v, int l, int r) {
-        int m = l + r >> 1;
-        apply1(v << 1, st[v].lz, l, m);
-        apply1(v << 1 | 1, st[v].lz, m + 1, r);
-        st[v].lz = 0;
-        apply2(v << 1, st[v].mn1, l == m);
-        apply2(v << 1 | 1, st[v].mn1, m + 1 == r);
-        apply3(v << 1, st[v].mx1, l == m);
-        apply3(v << 1 | 1, st[v].mx1, m + 1 == r);
-    }
-    void pull(int v) {
-        st[v].sum = st[v << 1].sum + st[v << 1 | 1].sum;
-        if (st[v << 1].mn1 == st[v << 1 | 1].mn1) {
-            st[v].mn1 = st[v << 1].mn1;
-            st[v].mncnt = st[v << 1].mncnt + st[v << 1 | 1].mncnt;
-            st[v].mn2 = min(st[v << 1].mn2, st[v << 1 | 1].mn2);
-        } else if (st[v << 1].mn1 > st[v << 1 | 1].mn1) {
-            st[v].mn1 = st[v << 1 | 1].mn1;
-            st[v].mncnt = st[v << 1 | 1].mncnt;
-            st[v].mn2 = min(st[v << 1].mn1, st[v << 1 | 1].mn2);
-        } else {
-            st[v].mn1 = st[v << 1].mn1;
-            st[v].mncnt = st[v << 1].mncnt;
-            st[v].mn2 = min(st[v << 1].mn2, st[v << 1 | 1].mn1);
-        }
-        if (st[v << 1].mx1 == st[v << 1 | 1].mx1) {
-            st[v].mx1 = st[v << 1].mx1;
-            st[v].mxcnt = st[v << 1].mxcnt + st[v << 1 | 1].mxcnt;
-            st[v].mx2 = max(st[v << 1].mx2, st[v << 1 | 1].mx2);
-        } else if (st[v << 1].mx1 < st[v << 1 | 1].mx1) {
-            st[v].mx1 = st[v << 1 | 1].mx1;
-            st[v].mxcnt = st[v << 1 | 1].mxcnt;
-            st[v].mx2 = max(st[v << 1].mx1, st[v << 1 | 1].mx2);
-        } else {
-            st[v].mx1 = st[v << 1].mx1;
-            st[v].mxcnt = st[v << 1].mxcnt;
-            st[v].mx2 = max(st[v << 1].mx2, st[v << 1 | 1].mx1);
-        }
-    }
-    void upd1(int v, int l, int r, int ql, int qr, int x) {
+    void upd(int v, int l, int r, int ql, int qr, int op, int h) {
         if (l > qr || r < ql) return;
         if (l >= ql && r <= qr) {
-            apply1(v, x, l, r);
+            if (v < sz) change(v, h, op + 1);
+            else {
+                if (op == 2) st[v] = min(st[v], h);
+                else st[v] = max(st[v], h);
+            }
             return;
         }
-        push(v, l, r);
+        if (o[v] != -1) push(v);
         int m = l + r >> 1;
-        upd1(v << 1, l, m, ql, qr, x);
-        upd1(v << 1 | 1, m + 1, r, ql, qr, x);
-        pull(v);
+        upd(v << 1, l, m, ql, qr, op, h);
+        upd(v << 1 | 1, m + 1, r, ql, qr, op, h);
     }
-    void upd2(int v, int l, int r, int ql, int qr, int x) {
-        if (l > qr || r < ql || st[v].mn1 >= x) return;
-        if (l >= ql && r <= qr && st[v].mn2 > x) {
-            apply2(v, x, l == r);
-            return;
-        }
-        push(v, l, r);
-        int m = l + r >> 1;
-        upd2(v << 1, l, m, ql, qr, x);
-        upd2(v << 1 | 1, m + 1, r, ql, qr, x);
-        pull(v);
+    void pushdown(int v) {
+        if (v >= sz) return;
+        if (o[v] != -1) push(v);
+        pushdown(v << 1);
+        pushdown(v << 1 | 1);
     }
-    void upd3(int v, int l, int r, int ql, int qr, int x) {
-        if (l > qr || r < ql || st[v].mx1 <= x) return;
-        if (l >= ql && r <= qr && st[v].mx2 < x) {
-            apply3(v, x, l == r);
-            return;
-        }
-        push(v, l, r);
-        int m = l + r >> 1;
-        upd3(v << 1, l, m, ql, qr, x);
-        upd3(v << 1 | 1, m + 1, r, ql, qr, x);
-        pull(v);
+    void upd(int l, int r, int op, int h) {
+        upd(1, 0, sz - 1, l, r, op, h);
     }
-    void pushdown(int v, int l, int r) {
-        if (l == r) return;
-        push(v, l, r);
-        int m = l + r >> 1;
-        pushdown(v << 1, l, m);
-        pushdown(v << 1 | 1, m + 1, r);
-    }
-    int qry(int v, int l, int r, int ql, int qr) {
-        if (l > qr || r < ql) return 0;
-        if (l >= ql && r <= qr) return st[v].sum;
-        push(v, l, r);
-        int m = l + r >> 1;
-        return qry(v << 1, l, m, ql, qr) + qry(v << 1 | 1, m + 1, r, ql, qr);
-    }
-    void pushdown() {
-        pushdown(1, 0, sz - 1);
-    }
-    void upd1(int l, int r, int x) {
-        upd1(1, 0, sz - 1, l, r, x);
-    }
-    void upd2(int l, int r, int x) {
-        upd2(1, 0, sz - 1, l, r, x);
-    }
-    void upd3(int l, int r, int x) {
-        upd3(1, 0, sz - 1, l, r, x);
-    }
-    int qry(int l, int r) {
-        return qry(1, 0, sz - 1, l, r);
-    }
-    int get(int id) {
-        return st[id + sz].sum;
+    int get(int i) {
+        return st[i + sz];
     }
 };
 
-signed main() {
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n, q;
-    cin >> n >> q;
-    vector<int> a(n);
-    SegmentTreeBeats st(a);
-    while (q--) {
-        int tp;
-        cin >> tp;
-        if (tp == 2) {
-            int l, r, x;
-            cin >> l >> r >> x;
-            st.upd3(l, r, x);
-        } else {
-            int l, r, x;
-            cin >> l >> r >> x;
-            st.upd2(l, r, x);
-        }
+    int n, k;
+    cin >> n >> k;
+    segtree st(n);
+    for (int i = 0; i < k; i++) {
+        int op, l, r, h;
+        cin >> op >> l >> r >> h;
+        st.upd(l, r, op, h);
     }
-    st.pushdown();
+    st.pushdown(1);
     for (int i = 0; i < n; i++) cout << st.get(i) << '\n';
 }
