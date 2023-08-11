c0c5a622c6f3591ea1350aabd18a1cecdda398d5
Desh Acharjee <134647238+DeshAcharjee@users.noreply.github.com>
2023-08-11 14:46:57 +0600

    Update JOI2023_Stone_Arranging_2.cpp

--- JOI/JOI2023_Stone_Arranging_2.cpp
+++ JOI/JOI2023_Stone_Arranging_2.cpp
@@ -7,12 +7,10 @@ int main() {
     cin.tie(0);
     int n;
     cin >> n;
-    vector<int> a(n), b(n);
+    vector<int> a(n);
     map<int, int> r;
     for (int i = 0; i < n; i++) cin >> a[i], r[a[i]] = i;
     for (int i = 0; i < n; i = r[a[i]] + 1)
-        for (int j = i; j <= r[a[i]]; j++) 
-            b[j] = a[i];
-    for (int s : b) cout << s << '\n';
+        for (int j = i; j <= r[a[i]]; j++) cout << a[i] << '\n';
     return 0;
 }
