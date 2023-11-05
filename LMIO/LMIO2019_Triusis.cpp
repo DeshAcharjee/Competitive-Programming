c709b2a2f3f50d1c8161ee9416ca76950227821f
Desh Acharjee <134647238+DeshAcharjee@users.noreply.github.com>
2023-11-06 01:28:33 +0600

    Update LMIO2019_Triusis.cpp

--- LMIO/LMIO2019_Triusis.cpp
+++ LMIO/LMIO2019_Triusis.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-signed main() {
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int n, m, ans = 0;
     cin >> n >> m;
     vector<int> a(n), b;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++)
-        if (a[i] <= (i + 1) * m)
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] <= (i + 1) * m) {
             b.push_back((i + 1) * m - a[i]);
-    multiset<int> lis;
+        }
+    }
+    vector<int> lis;
     for (int x : b) {
-        auto it = lis.upper_bound(x);
-        if (it != lis.end()) lis.erase(it);
-        lis.insert(x);
+        auto it = upper_bound(lis.begin(), lis.end(), x);
+        if (it == lis.end()) lis.push_back(x);
+        else *it = x;
     }
     cout << n - lis.size() << '\n';
+    return 0;
 }
